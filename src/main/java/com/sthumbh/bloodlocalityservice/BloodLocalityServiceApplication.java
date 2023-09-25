5aea90fa15c13fe99790e901c4965d28f8848d20
Mohammad Shahruk <mshahruk57@gmail.com>
2023-09-25 16:25:35 +0530

    initial commit

---
+++ src/main/java/com/sthumbh/bloodlocalityservice/BloodLocalityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodlocalityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodLocalityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodLocalityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sthumbh/bloodlocalityservice/BloodLocalityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodlocalityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodLocalityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
